273a9402c07212bfa3129cdaf9c94acab3831709
Lizzie Avila <33301810+lizzieavila@users.noreply.github.com>
2018-10-01 14:52:14 -0300

    Add files via upload

---
+++ calccontroler.js
@@ -0,0 +1,55 @@
+class CalcControler{
+
+	constructor(){
+		this._locale = 'pt-BR';
+		this._displayCalcEl = document.querySelector("#display");
+		this._dateEl = document.querySelector("#data");
+		this._timeEl = document.querySelector("#hora");		
+		this._currentDate; 
+		this.initialize();
+	}
+	initialize(){
+		this.setdisplaydatetime();
+
+		setInterval(()=>{
+			this.setdisplaydatetime();
+			
+		},1000);
+	}
+
+	setdisplaydatetime(){
+		//this.displayDate = this.currentDate.toLocaleDateString('pt-BR');
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+	}
+
+	get displayTime(){
+		return this._timeEl.innerHTML;
+
+	}
+	set displayTime(Value){
+		this._timeEl.innerHTML = Value;
+
+    }
+	get displayDate(){
+		return this._dateEl.innerHTML;
+
+	}
+	set displayDate(Value){
+		this._dateEl.innerHTML = Value;
+	}
+	get displayCalc(){
+		return this._displayCalcEl.innerHTML;
+	}
+	set displayCalc(Value){
+		this._displayCalcEl.innerHTML = Value;
+	}
+	get currentDate(){
+		return new Date();
+	}
+	set currentDate(value){
+		this._currentDate = Value;
+ 
+	}
+
+}
